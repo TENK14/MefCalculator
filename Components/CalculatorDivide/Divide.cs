bbc0b817d0d3268c9a9d31b7c8c29e3ab03c2c25
wagner <wagner@TOMAS-WAGNER>
2017-03-23 15:37:24 +0100

    Add project files.

---
+++ Components/CalculatorDivide/Divide.cs
@@ -0,0 +1,22 @@
+﻿using MefCalculatorContract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDivide
+{
+    [Export(typeof(ICalculator))]
+    [ExportMetadata("CalciMetaData", "Divide")]
+    public class Divide : ICalculator
+    {
+        #region Interface members
+        public int GetNumber(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        #endregion
+    }
+}
---
+++ Components/CalculatorSubtract/Subtract.cs
@@ -0,0 +1,22 @@
+﻿using MefCalculatorContract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorSubtraction
+{
+    [Export(typeof(ICalculator))]
+    [ExportMetadata("CalciMetaData", "Subtract")]
+    public class Subtract : ICalculator
+    {
+        #region Interface members
+        public int GetNumber(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        #endregion
+    }
+}
